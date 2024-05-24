8b5d1d6dea8153e74c10aef4470d8fb9b0c6bd08
mihirsasmal <mihirsasmal@gmail.com>
2024-05-24 11:42:36 +0100

    Added retry option for test run

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: true,
   projectId: "sda137",
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
